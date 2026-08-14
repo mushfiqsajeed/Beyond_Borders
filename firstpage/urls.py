b63a0c305eb4b44f50dbc5d67aff4391ad4273e9
Mushfiq Sajeed Hossain <mushfiqsajid.2003@gmail.com>
2026-08-14 18:42:57 +0600

    Connected Database and created login and signup page with the help of HTML, CSS, and Django.

--- firstpage/urls.py
+++ firstpage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello)
-
+    path('', views.hello),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup, name='signup')
 ]
